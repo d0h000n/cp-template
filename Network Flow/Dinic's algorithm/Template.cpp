5ffe76d1aab1d730e0e71359c8e05ee21000284c
Dohoon Kim <70815390+d0h000n@users.noreply.github.com>
2023-10-04 13:54:55 +0900

    Update Template.cpp

--- Network Flow/Dinic's algorithm/Template.cpp
+++ Network Flow/Dinic's algorithm/Template.cpp
@@ -5,12 +5,13 @@ struct max_flow {
     T cap[V][V], flw[V][V];
     max_flow(int s, int t): src(s), snk(t) {}
     void add(int u, int v, T f) {
-        if (!f or (cap[u][v]+=f) > f or cap[v][u]) return;
+        cap[u][v] += f;
+        if (f == 0 or cap[u][v] > f or cap[v][u] > 0) return;
         adj[u].push_back(v), adj[v].push_back(u);
     }
     T augment(int s, T a=INF) {
         if (s == snk) return a;
-        for (;not empty(ldj[s]); ldj[s].pop_back()) {
+        for (;not ldj[s].empty(); ldj[s].pop_back()) {
             int u = ldj[s].back();
             if (cap[s][u] <= flw[s][u]) continue;
             T r = augment(u, min(a,cap[s][u]-flw[s][u]));
@@ -25,18 +26,18 @@ struct max_flow {
     bool build() {
         queue<int> q;
         rep(i,0,V-1) ldj[i].clear(), dist[i]=1e9;
-        dist[src]=1, q.push(src);
-        while (not empty(q)) {
+        dist[src]=0, q.push(src);
+        while (not q.empty()) {
             int s = q.front(); q.pop();
             for (auto u : adj[s]) if (cap[s][u] > flw[s][u]) {
-                if (dist[u]!=int(1e9)) dist[u] = dist[s]+1, q.push(u);
+                if (dist[u] == int(1e9)) dist[u] = dist[s]+1, q.push(u);
                 if (dist[u] == dist[s]+1) ldj[s].push_back(u);
             }
         }
-        return dist[snk]!=int(1e9);
+        return dist[snk] != int(1e9);
     }
     T get() {
-        T res = 0;
+        T res=0;
         while (build()) for (T d; (d=augment(src)); res += d);
         return res;
     }
