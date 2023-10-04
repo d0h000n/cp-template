1bf43810d7526545d2b345dc528a53b86f9da948
Dohoon Kim <70815390+d0h000n@users.noreply.github.com>
2023-10-04 15:22:40 +0900

    Update Template.cpp

--- Network Flow/Dinic's algorithm/Template.cpp
+++ Network Flow/Dinic's algorithm/Template.cpp
@@ -2,22 +2,22 @@ template <const int V, typename T, const T INF>
 struct max_flow {
     vector<int> adj[V], ldj[V];
     int src, snk, dist[V];
-    T cap[V][V], flw[V][V];
+    T res[V][V];
     max_flow(int s, int t): src(s), snk(t) {}
     void add(int u, int v, T f) {
-        cap[u][v] += f;
-        if (f == 0 or cap[u][v] > f or cap[v][u] > 0) return;
+        res[u][v] += f;
+        if (f == 0 or res[u][v] > f or res[v][u] > 0) return;
         adj[u].push_back(v), adj[v].push_back(u);
     }
     T augment(int s, T a=INF) {
         if (s == snk) return a;
         for (;not ldj[s].empty(); ldj[s].pop_back()) {
             int u = ldj[s].back();
-            if (cap[s][u] <= flw[s][u]) continue;
-            T r = augment(u, min(a,cap[s][u]-flw[s][u]));
+            if (!res[s][u]) continue;
+            T r = augment(u, min(a,res[s][u]));
             if (r > 0) {
-                flw[s][u] += r;
-                flw[u][s] -= r;
+                res[s][u] -= r;
+                res[u][s] += r;
                 return r;
             }
         }
@@ -29,7 +29,7 @@ struct max_flow {
         dist[src]=0, q.push(src);
         while (not q.empty()) {
             int s = q.front(); q.pop();
-            for (auto u : adj[s]) if (cap[s][u] > flw[s][u]) {
+            for (auto u : adj[s]) if (res[s][u] > 0) {
                 if (dist[u] == int(1e9)) dist[u] = dist[s]+1, q.push(u);
                 if (dist[u] == dist[s]+1) ldj[s].push_back(u);
             }
@@ -37,8 +37,8 @@ struct max_flow {
         return dist[snk] != int(1e9);
     }
     T get() {
-        T res=0;
-        while (build()) for (T d; (d=augment(src)); res += d);
-        return res;
+        T r=0;
+        while (build()) for (T d; (d=augment(src)); r += d);
+        return r;
     }
 };
